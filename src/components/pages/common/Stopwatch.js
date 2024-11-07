cc2f650095c7f4795782b8c41c64198251e90706
Vladulescu Stefan <s.vladulescu@gmail.com>
2024-11-07 17:49:10 +0200

    Added the option to hide the stopwatch and only show a small centered rectangle at the top of the screen with the current timer time that expands back to the stopwatch when clicked

--- src/components/pages/common/Stopwatch.js
+++ src/components/pages/common/Stopwatch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { formatTime } from "../../../helpers";
-import './stylings/stopwatch.css'
+import './stylings/stopwatch.css';
 import closeIcon from '../../../assets/close.svg';
 import minimizeIcon from '../../../assets/minimise.svg';
 import maximizeIcon from '../../../assets/maximise.svg';
@@ -9,82 +9,77 @@ import resetIcon from '../../../assets/restart.svg';
 import lapIcon from '../../../assets/lap.svg';
 import pauseIcon from '../../../assets/pause.svg';
 
-
-
-
-
-
-
-
-
-const Stopwatch = ({closeLogWindow}) => {
-
+const Stopwatch = ({ closeLogWindow }) => {
     const [laps, setLaps] = useState([]);
     const [isStarted, setIsStarted] = useState(false);
-    const [isMinimized, setIsMinimized] = useState(false)
+    const [isMinimized, setIsMinimized] = useState(false);
+    const [isHidden, setIsHidden] = useState(false);
     const [seconds, setSeconds] = useState(0);
 
     useEffect(() => {
-        if(isStarted){
-            const timer = setInterval(() => {
+        let timer;
+        if (isStarted) {
+            timer = setInterval(() => {
                 setSeconds((prevSeconds) => prevSeconds + 1);
             }, 1000);
-        
-            return () => clearInterval(timer);
         }
-        
+        return () => clearInterval(timer);
     }, [isStarted]);
 
-
-    
-    const handleStart = () =>{
-        setIsStarted(true);
-    }
-    const handlePause = () =>{
-        setIsStarted(false);
-    }
-    const handleAddLap = () =>{
-        setLaps((laps)=>[...laps, formatTime(seconds)]);
-    }
-    const handleReset = () =>{
+    const handleStart = () => setIsStarted(true);
+    const handlePause = () => setIsStarted(false);
+    const handleAddLap = () => setLaps((laps) => [...laps, formatTime(seconds)]);
+    const handleReset = () => {
         setLaps([]);
         setSeconds(0);
         setIsStarted(false);
-    }
-    const handleMinimize = () =>{
-        setIsMinimized(true)
-    }
-    const handleMaximize = () =>{
-        setIsMinimized(false)
-    }
-    return ( 
-        <div className={`stopwatch ${isMinimized ? 'minimized-stopwatch' : ''}`}>
-            <div className="top-bar">
-                {isMinimized ? (<button onClick={handleMaximize}><img src={maximizeIcon} alt=""></img></button>) : 
-                    <button onClick={handleMinimize}><img src={minimizeIcon} alt=""></img></button>
-                }
-                <h1>Stopwatch</h1>
-                <button onClick={closeLogWindow}><img src={closeIcon} alt=""></img></button>
-            </div>
-            <div className="stopwatch-time">
-                {formatTime(seconds)}
-            </div>
-            <h3>Laps</h3>
-            <div className="laps-container">
-                {laps.length > 0 ? laps.map((lap, index)=>(<div className="lap"><p>Lap {index+1}.</p><p>{lap}</p></div>)) : ''}
-            </div>
-            <div className="stopwatch-buttons">
-                <button onClick={handleReset}><img className="" src={resetIcon} alt=""></img></button>
-                <button onClick={handleAddLap}><img src={lapIcon} alt=""></img></button>
+    };
+    const handleMinimize = () => setIsMinimized(true);
+    const handleMaximize = () => setIsMinimized(false);
 
-
-                {isStarted ? (<button onClick={handlePause}><img src={pauseIcon} alt=""></img></button>) : (<button onClick={handleStart}><img src={playIcon} alt=""></img></button>)}
-                
-                
-                
-            </div>
+    return (
+        <div className={`stopwatch ${isMinimized ? 'minimized-stopwatch' : ''} ${isHidden ? 'hide-stopwatch' : ''}`}>
+            {isHidden ? (
+                <div className="hidden-stopwatch-timer" onClick={()=>setIsHidden(false)}>
+                    {formatTime(seconds)}
+                </div>
+            ) : (
+                <>
+                    <div className="top-bar">
+                        {isMinimized ? (
+                            <button onClick={handleMaximize}><img src={maximizeIcon} alt="" /></button>
+                        ) : (
+                            <button onClick={handleMinimize}><img src={minimizeIcon} alt="" /></button>
+                        )}
+                        <h1>Stopwatch</h1>
+                        <button onClick={() => setIsHidden(true)} className="small-icon"><img src="/icons/minus.svg" alt="" /></button>
+                        <button onClick={closeLogWindow}><img src={closeIcon} alt="" /></button>
+                    </div>
+                    <div className="stopwatch-time">
+                        {formatTime(seconds)}
+                    </div>
+                    <h3>Laps</h3>
+                    <div className="laps-container">
+                        {laps.length > 0 ? laps.map((lap, index) => (
+                            <div className="lap" key={index}>
+                                <p>Lap {index + 1}.</p>
+                                <p>{lap}</p>
+                            </div>
+                        )) : null}
+                    </div>
+                    <div className="stopwatch-buttons">
+                        <button onClick={handleReset}><img src={resetIcon} alt="" /></button>
+                        <button onClick={handleAddLap}><img src={lapIcon} alt="" /></button>
+                        {isStarted ? (
+                            <button onClick={handlePause}><img src={pauseIcon} alt="" /></button>
+                        ) : (
+                            <button onClick={handleStart}><img src={playIcon} alt="" /></button>
+                        )}
+                    </div>
+                </>
+            )}
         </div>
-     );
-}
- 
+    );
+};
+
 export default Stopwatch;
