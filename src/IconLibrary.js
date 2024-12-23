9dab65011aecf349cd73af32308f3c112e843a02
Stefan0712 <s.vladulescu@gmail.com>
2024-12-23 23:21:23 +0200

    Finished the Manage Goal component's ui, without any functionality yet. Added new icons to IconLibrary

--- src/IconLibrary.js
+++ src/IconLibrary.js
@@ -1,7 +1,10 @@
 import PlayIcon from './assets/play.svg';
 import BackIcon from './assets/back-arrow.svg';
 import EditIcon from './assets/edit.svg';
-import DeleteIcon from './assets/delete.svg'
+import DeleteIcon from './assets/delete.svg';
+import SaveIcon from './assets/save.svg';
+import YesIcon from './assets/yes.svg';
+import NoIcon from './assets/no.svg';
 
 
 
@@ -9,5 +12,8 @@ export const IconLibrary = {
     PlayIcon,
     BackIcon,
     EditIcon,
-    DeleteIcon
+    DeleteIcon,
+    SaveIcon,
+    YesIcon,
+    NoIcon
 }
--- src/components/pages/common/ManageGoalBody.js
+++ src/components/pages/common/ManageGoalBody.js
@@ -5,25 +5,42 @@ import { makeFirstUpperCase } from "../../../helpers";
 
 
 
-const ManageGoalBody = ({goal, showLog}) => {
+const ManageGoalBody = ({goal}) => {
 
     const [showEdit, setShowEdit] = useState(false);
+    const [confirmButtons, setConfirmButtons] = useState(false);
 
 
 
     
     return ( 
-        <button className='goal-body' onClick={()=>showLog(goal.name)} key={goal.name}>
-            <img className='small-icon' src={goal.icon} alt=''></img>
-            <p>{makeFirstUpperCase(goal.name)}</p>
+        <div className={`goal-body ${showEdit ? 'show-edit' : ''}`} key={goal.name}>
+            <div className="goal-body-top">
+                <img className='small-icon' src={goal.icon} alt=''></img>
+                <p>{makeFirstUpperCase(goal.name)}</p>
+                <div className="goal-buttons">
+                    <img onClick={()=>setShowEdit((showEdit)=>!showEdit)} className='small-icon' src={IconLibrary.EditIcon}></img>
+                    
+                </div>
+            </div>
             <div className="edit-inputs">
                 <input type='number' value={goal.target}></input>
                 <input type='text' value={goal.unit}></input>
+                <img className='small-icon' src={IconLibrary.SaveIcon}></img>
+                <div className="delete-container">
+                    
+                        <div className={`confirm-buttons ${confirmButtons ? 'show-confirm' : ''}`}>
+                            <img className='small-icon' src={IconLibrary.YesIcon}></img>
+                            <img className='small-icon' src={IconLibrary.NoIcon} onClick={()=>setConfirmButtons(false)}></img>
+                        </div>
+                    
+                        <img className={`delete-button small-icon ${confirmButtons ? 'hide' : ''}`} src={IconLibrary.DeleteIcon} onClick={()=>setConfirmButtons(true)}></img>
+                    
+                </div>
+                
             </div>
-            <img className='small-icon' src={IconLibrary.EditIcon}></img>
-            <img className='small-icon' src={IconLibrary.DeleteIcon}></img>
             
-        </button>
+        </div>
      );
 }
  
