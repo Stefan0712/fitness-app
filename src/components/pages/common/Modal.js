793dce489b7e4889aa71d5668431c2fdbb81208e
Stefan0712 <s.vladulescu@gmail.com>
2024-12-23 21:16:16 +0200

    Updated the modal. Simplified the modal, changed the layout and removed lines and colors what were not useful

--- src/components/pages/common/Modal.js
+++ src/components/pages/common/Modal.js
@@ -5,13 +5,11 @@ import './stylings/modal.css';
 const Modal = ({type, title, message, positiveOnClick, negativeOnClick, confirmationOnClick}) => {
     return ( 
         <div className={`modal ${type}`}>
-            <div className="modal-header">
-                <h2>{title}</h2>
-            </div>
+             <div className='modal-title'>{title}</div>   
             <div className="modal-message"><p>{message}</p></div>
             <div className="modal-buttons">
                 {positiveOnClick ? <button onClick={positiveOnClick} className="green-button">Confirm</button> : ''}
-                {negativeOnClick ? <button onClick={negativeOnClick} className="red-button">Cancel</button> : ''}
+                {negativeOnClick ? <button onClick={negativeOnClick} className="cancel-button">Cancel</button> : ''}
                 {confirmationOnClick ? <button onClick={confirmationOnClick} className="ok-button">OK</button> : ''}
             </div>
         </div>
