d9fe8f53c25c59cd1cec86cf9b83b00f91af4720
Stefan0712 <s.vladulescu@gmail.com>
2024-12-23 21:32:14 +0200

    Removed goals from the main quick menu and added two new screens, one with a list of daily goals where you can log them, and one where you can see a list of them with target, from which you can edit them, delete them, or create new ones

--- src/components/pages/common/QuickMenu.js
+++ src/components/pages/common/QuickMenu.js
@@ -3,46 +3,96 @@ import closeIcon from '../../../assets/close.svg';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { makeFirstUpperCase } from '../../../helpers';
+import { useState } from 'react';
 
 
 const QuickMenu = ({closeQuickmenu, showLog}) => {
     const dailyGoals = useSelector((state)=> state.user.userData.goals);
+
+    const [quickMenuScreen, setQuickMenuScreen] = useState('menu')
 
     return ( 
         <div className="quick-menu">
             <button id='quick-menu-close-btn' onClick={closeQuickmenu}>
                 <img src={closeIcon} alt=''></img>
             </button>
-            <h1>Quick Menu</h1>
-            <div className='new-items-buttons'>
-                <Link to={'/create-workout'} className="orange-button large-button">Create Workout</Link>
-                <Link to={'/create-exercise'} className="orange-button large-button">Create Exercise</Link>
-            </div>
-            <h2>Tools</h2>
-            <button className='quick-button' onClick={()=>showLog('stopwatch')}>
-                <img src={'/icons/stopwatch.svg'} alt=''></img>
-                <p>Stopwatch</p>
-            </button>
-            <h2>Quick Logs</h2>
-            <button className='quick-button' onClick={()=>showLog("food")} key={"food"}>
-                <img src='/icons/food.svg' alt=''></img>
-                <p>Food</p>
-            </button>
-            <button className='quick-button' onClick={()=>showLog('exercise')} key={'exercise'}>
-                <img src='/icons/exercise.svg' alt=''></img>
-                <p>Exercise</p>
-            </button>
+            {quickMenuScreen === 'menu' ? (
+
+                <div className='quick-menu-screen'>
+                    <h1>Quick Menu</h1>
+                    <div className='new-items-buttons'>
+                        <Link to={'/create-workout'} className="orange-button large-button">Create Workout</Link>
+                        <Link to={'/create-exercise'} className="orange-button large-button">Create Exercise</Link>
+                    </div>
+                    <h2>Tools</h2>
+                    <button className='quick-button' onClick={()=>showLog('stopwatch')}>
+                        <img src={'/icons/stopwatch.svg'} alt=''></img>
+                        <p>Stopwatch</p>
+                    </button>
+                    <h2>Quick Logs</h2>
+                    <button className='quick-button' onClick={()=>showLog("food")} key={"food"}>
+                        <img src='/icons/food.svg' alt=''></img>
+                        <p>Food</p>
+                    </button>
+                    <button className='quick-button' onClick={()=>showLog('exercise')} key={'exercise'}>
+                        <img src='/icons/exercise.svg' alt=''></img>
+                        <p>Exercise</p>
+                    </button>
+
+
+                    <h2>Goals</h2>
+                    <button className='quick-button' onClick={()=>setQuickMenuScreen('log-goals')} key={'exercise'}>
+                        <img src='/icons/exercise.svg' alt=''></img>
+                        <p>Log Goals</p>
+                    </button>
+                    <button className='quick-button' onClick={()=>setQuickMenuScreen('manage-goals')} key={'exercise'}>
+                        <img src='/icons/exercise.svg' alt=''></img>
+                        <p>Manage Goals</p>
+                    </button>
+                </div>
+            ) : ''}
+            {quickMenuScreen === 'log-goals' ? (
+
+                <div className='quick-menu-screen'>
+                    <button onClick={()=>setQuickMenuScreen('menu')}>Back</button>
+                    <h1>Log Goals</h1>
+                    <button className='quick-button' onClick={()=>showLog("food")} key={"food"}>
+                        <img src='/icons/food.svg' alt=''></img>
+                        <p>Food</p>
+                    </button>
+                    <button className='quick-button' onClick={()=>showLog('exercise')} key={'exercise'}>
+                        <img src='/icons/exercise.svg' alt=''></img>
+                        <p>Exercise</p>
+                    </button>
+                    {dailyGoals?.map((goal)=>(
+                        <button className='quick-button' onClick={()=>showLog(goal.name)} key={goal.name}>
+                            <img src={goal.icon} alt=''></img>
+                            <p>{makeFirstUpperCase(goal.name)}</p>
+                        </button>
+                    ))}
+                </div>
+            ) : ''}
+            {quickMenuScreen === 'manage-goals' ? (
+
+                <div className='quick-menu-screen'>
+                    <button onClick={()=>setQuickMenuScreen('menu')}>Back</button>
+                    <h1>Manage Goals</h1>
+                    <Link to={'/create-workout'} className="orange-button large-button">Create Goal</Link>
+                    {dailyGoals?.map((goal)=>(
+                        <button className='goal-body' onClick={()=>showLog(goal.name)} key={goal.name}>
+                            <img className='medium-icon' src={goal.icon} alt=''></img>
+                            <p>{makeFirstUpperCase(goal.name)}</p>
+                        </button>
+                    ))}   
+                    
+                </div>
+            ) : ''}
+            
             
            
-            <h2>Goals</h2>
-            {dailyGoals?.map((goal)=>(
-                <button className='quick-button' onClick={()=>showLog(goal.name)} key={goal.name}>
-                    <img src={goal.icon} alt=''></img>
-                    <p>{makeFirstUpperCase(goal.name)}</p>
-                </button>
-            ))}
+            
         </div>
-     );
+    );
 }
  
 export default QuickMenu;
