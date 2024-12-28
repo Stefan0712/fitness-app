00313feb9acf1c44e9e14bd61cc1d430c484b779
Stefan0712 <s.vladulescu@gmail.com>
2024-12-28 10:14:16 +0200

    Added all icons from the assets folder inside IconLibrary.js and removed Icon from the names of the icons because it's self explanatory that it's an icon

--- src/IconLibrary.js
+++ src/IconLibrary.js
@@ -1,29 +1,106 @@
-import PlayIcon from './assets/play.svg';
-import BackIcon from './assets/back-arrow.svg';
-import EditIcon from './assets/edit.svg';
-import DeleteIcon from './assets/delete.svg';
-import SaveIcon from './assets/save.svg';
-import YesIcon from './assets/yes.svg';
-import NoIcon from './assets/no.svg';
-import AddIcon from './assets/add.svg';
-import SettingsIcon from './assets/settings.svg';
-import DownloadIcon from './assets/download.svg'
-import MinusIcon from './assets/substract.svg';
-import ArrowIcon from './assets/arrow.svg';
+import Play from './assets/play.svg';
+import Back from './assets/back-arrow.svg';
+import Edit from './assets/edit.svg';
+import Delete from './assets/delete.svg';
+import Save from './assets/save.svg';
+import Yes from './assets/yes.svg';
+import No from './assets/no.svg';
+import Add from './assets/add.svg';
+import Settings from './assets/settings.svg';
+import Download from './assets/download.svg'
+import Minus from './assets/substract.svg';
+import Arrow from './assets/arrow.svg';
+
+
+
+
+
+import BackArrow from './assets/back-arrow.svg';
+import Calories from './assets/calories.svg';
+import Checkmark from './assets/checkmark.svg';
+import CircleCheckmark from './assets/circle-checkmark.svg';
+import Clipboard from './assets/checkmark.svg';
+import Close from './assets/close.svg';
+import Dumbbell from './assets/dumbbell.svg';
+import Exercise from './assets/exercise.svg';
+import Filter from './assets/filter.svg';
+import Folder from './assets/folder.svg';
+import Food from './assets/food.svg';
+import Home from './assets/food.svg';
+import Lap from './assets/lap.svg';
+import Link from './assets/link.svg';
+import List from './assets/list.svg';
+import Loading from './assets/loading.svg';
+import Maximize from './assets/maximise.svg';
+import Minimize from './assets/minimise.svg';
+import Muscle from './assets/muscle.svg';
+import Next from './assets/next.svg';
+import Previous from './assets/previous.svg';
+import Pause from './assets/pause.svg';
+import Plus from './assets/plus.svg';
+import PlusCircle from './assets/plus-circle.svg';
+import Profile from './assets/profile.svg';
+import Restart from './assets/restart.svg';
+import Sleep from './assets/sleep.svg';
+import Start from './assets/start.svg';
+import Steps from './assets/steps.svg';
+import Stop from './assets/stop.svg';
+import Stopwatch from './assets/stopwatch.svg';
+import Substract from './assets/substract.svg';
+import Time from './assets/time.svg';
+import Water from './assets/water.svg';
+import Weight from './assets/weight.svg';
+import Workout from './assets/workout.svg';
 
 
 
 export const IconLibrary = {
-    PlayIcon,
-    BackIcon,
-    EditIcon,
-    DeleteIcon,
-    SaveIcon,
-    YesIcon,
-    NoIcon,
-    AddIcon,
-    SettingsIcon,
-    DownloadIcon,
-    MinusIcon,
-    ArrowIcon
+    Play,
+    Back,
+    Edit,
+    Delete,
+    Save,
+    Yes,
+    No,
+    Add,
+    Settings,
+    Download,
+    Minus,
+    Arrow,
+    BackArrow,
+    Calories,
+    Checkmark,
+    CircleCheckmark,
+    Clipboard,
+    Close,
+    Dumbbell,
+    Exercise,
+    Filter,
+    Folder,
+    Food,
+    Home,
+    Lap,
+    Link,
+    List,
+    Loading,
+    Maximize,
+    Minimize,
+    Muscle,
+    Next,
+    Previous,
+    Pause,
+    Plus,
+    PlusCircle,
+    Profile,
+    Restart,
+    Sleep,
+    Start,
+    Steps,
+    Stop,
+    Stopwatch,
+    Substract,
+    Time,
+    Water,
+    Weight,
+    Workout
 }
