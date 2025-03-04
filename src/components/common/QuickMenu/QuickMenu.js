f1d47fffb9064afed4dae7ae435fb2a022260a61
Stefan0712 <s.vladulescu@gmail.com>
2025-03-04 02:24:13 +0200

    CHanged the quick menu to be more minimalist and easier to use

--- src/components/common/QuickMenu/QuickMenu.js
+++ src/components/common/QuickMenu/QuickMenu.js
@@ -28,41 +28,26 @@ const QuickMenu = ({closeQuickmenu, openGoals, openStopwatch, openExerciseLogs, 
 
 
     return ( 
-        <div className={styles["quick-menu"]} ref={quickMenuRef}>
-            <div className={styles.header}>
-                <h2>Quick Menu</h2>
-                <button className='clear-button' onClick={closeQuickmenu}>
-                    <img className='small-icon' src={IconLibrary.Close} alt=''></img>
-                </button>
-            </div>
-            {selectedScreen === 'menu' ? (
-
-                <div className={styles['quick-menu-screen']}>
-                    
-                    <button className={styles['quick-button']} onClick={openStopwatch}>
-                        <img src={IconLibrary.Stopwatch} alt=''></img>
-                        <p>Stopwatch</p>
-                    </button>
-                    <button className={styles['quick-button']} onClick={openFoodLogs} key={"food"}>
-                        <img src={IconLibrary.Food} alt=''></img>
-                        <p>Food</p>
-                    </button>
-                    <button className={styles['quick-button']} onClick={openExerciseLogs} key={'exercise'}>
-                        <img src={IconLibrary.Exercise} alt=''></img>
-                        <p>Exercise</p>
-                    </button>
-                    <button className={styles['quick-button']} onClick={openGoals} key={'exercise'}>
-                        <img src={IconLibrary.Goals} alt=''></img>
-                        <p>Goals</p>
-                    </button>
-                </div>
-            ) : ''}
-            
-           
-        
-            
-           
-            
+        <div className={styles["quick-menu"]} ref={quickMenuRef}>             
+            <button className={styles['quick-button']} onClick={openStopwatch}>
+                <img src={IconLibrary.Stopwatch} alt=''></img>
+                <p>Stopwatch</p>
+            </button>
+            <button className={styles['quick-button']} onClick={openFoodLogs} key={"food"}>
+                <img src={IconLibrary.Food} alt=''></img>
+                <p>Log Food</p>
+            </button>
+            <button className={styles['quick-button']} onClick={openExerciseLogs} key={'exercise'}>
+                <img src={IconLibrary.Exercise} alt=''></img>
+                <p>Log Exercise</p>
+            </button>
+            <button className={styles['quick-button']} onClick={openGoals} key={'exercise'}>
+                <img src={IconLibrary.Goals} alt=''></img>
+                <p>Goals</p>
+            </button> 
+            <button className={`${styles['quick-button']} ${styles['close-button']}`} onClick={closeQuickmenu}>
+                <p>Close</p>
+            </button> 
         </div>
     );
 }
