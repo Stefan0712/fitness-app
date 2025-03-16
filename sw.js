80efa0cb1a376b0d83df0abf1a8393bb6cb679a6
Stefan0712 <s.vladulescu@gmail.com>
2025-03-16 21:18:09 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'easyfit-cache-v1';
+const OFFLINE_PAGE = '/offline.html';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './',  // Root directory
+        './index.html',
+        './manifest.json',
+        './icon-192x192.png',
+        './icon-512x512.png',
+        './offline.html' // Add an offline fallback page
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request)
+          .then((networkResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            });
+          })
+          .catch(() => caches.match(OFFLINE_PAGE)) // Serve offline page if network fails
+      );
+    })
+  );
+});
