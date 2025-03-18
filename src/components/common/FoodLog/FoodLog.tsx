e6c9d6c5270de521214415190d50ee401777d869
Stefan0712 <s.vladulescu@gmail.com>
2025-03-18 19:28:40 +0200

    Removed labels and fieldsets from Food Log to make it more compact and changed the background color to the modal variable

--- src/components/common/FoodLog/FoodLog.tsx
+++ src/components/common/FoodLog/FoodLog.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-
+import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 import { addLog } from "../../../store/userSlice.ts";
 import { IconLibrary } from "../../../IconLibrary";
@@ -23,7 +23,6 @@ interface InputsData {
     sugar: number;
     calories: number;
     sodium: number;
-    isIncluded: boolean,
     time: string;
     type: string;
     note: string;
@@ -35,6 +34,9 @@ interface LogData{
     icon: string;
     data: InputsData
 }
+
+
+// TODO: Implement the feature to include values from foods to goals. Maybe add some default "goals" for each property/macro of foods that are hidden by default and let the user show them if they want. Use them for stats
 const FoodLog: React.FC<FoodLogProps> = ({closeMenu}) => {
 
     
@@ -43,20 +45,30 @@ const FoodLog: React.FC<FoodLogProps> = ({closeMenu}) => {
     const [currentScreen, setCurrentScreen] = useState<string>('log')
 
     const [name, setName] = useState<string>('');
-    const [qty, setQty] = useState<number>(0);
+    const [qty, setQty] = useState<number | string>('');
     const [unit, setUnit] = useState<string>('');
-    const [protein, setProtein] = useState<number>(0);
-    const [carbs, setCarbs] = useState<number>(0);
-    const [fats, setFats] = useState<number>(0);
-    const [sugar, setSugar] = useState<number>(0);
-    const [calories, setCalories] = useState<number>(0);
-    const [sodium, setSodium] = useState<number>(0);
+    const [protein, setProtein] = useState<number | string>('');
+    const [carbs, setCarbs] = useState<number | string>('');
+    const [fats, setFats] = useState<number | string>('');
+    const [sugar, setSugar] = useState<number | string>('');
+    const [calories, setCalories] = useState<number | string>('');
+    const [sodium, setSodium] = useState<number | string>('');
     const [time, setTime] = useState<string>('');
     const [type, setType] = useState<string>('unset');
     const [note, setNote] = useState<string>('');
-    const [includeInGoals, setIncludeInGoals] = useState<boolean>(true);
     
 
+
+    const convertToInt = (value: string | number): number =>{
+        if(typeof value === 'string'){
+            return parseInt(value);
+        }else if(typeof value === 'number'){
+            return value;
+        }else{
+            console.log("Invalid Value: ",value);
+            return 0;
+        }
+    }
     const handleLog = ()=>{
         const data: LogData = {
             id: uuidv4(),
@@ -65,15 +77,14 @@ const FoodLog: React.FC<FoodLogProps> = ({closeMenu}) => {
             icon: '/icons/food.svg',
             data: {
                 name,
-                qty,
-                unit,
-                protein,
-                carbs,
-                fats,
-                sugar,
-                calories,
-                sodium,
-                isIncluded: includeInGoals,
+                unit: unit,
+                qty: convertToInt(qty),
+                protein: convertToInt(protein),
+                carbs: convertToInt(carbs),
+                fats: convertToInt(fats),
+                sugar: convertToInt(sugar),
+                calories: convertToInt(calories),
+                sodium: convertToInt(sodium),
                 time,
                 type,
                 note
@@ -96,55 +107,24 @@ const FoodLog: React.FC<FoodLogProps> = ({closeMenu}) => {
                 {currentScreen === 'log' ? (
                     <div className={styles['log-screen']}>
                     <fieldset>
-                        <label>Name</label>
                         <input className={styles.name} type="text" name="name" id="name" onChange={(e) => setName(e.target.value)} value={name} required={true} placeholder="Name" />
                     </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Qty</label>
-                        <input type="number" name="qty" id="qty" onChange={(e) => setQty(e.target.value)} value={qty} placeholder="Qty" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Unit</label>
-                        <input type="text" name="unit" id="unit" onChange={(e) => setUnit(e.target.value)} value={unit} placeholder="Unit" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Type</label>
-                        <select name="type" id="type" onChange={(e) => setType(e.target.value)} value={type}>
+                        <input className={styles['half-input']} type="number" name="qty" id="qty" onChange={(e) => setQty(e.target.value)} value={qty} placeholder="Qty" />
+                        <input className={styles['half-input']} type="text" name="unit" id="unit" onChange={(e) => setUnit(e.target.value)} value={unit} placeholder="Unit" />
+                        <select className={styles['half-input']} name="type" id="type" onChange={(e) => setType(e.target.value)} value={type}>
                             <option value={'unset'}>Unset</option>
                             <option value={'breakfast'}>Breakfast</option>
                             <option value={'lunch'}>Lunch</option>
                             <option value={'dinner'}>Dinner</option>
                             <option value={'snack'}>Snack</option>
                         </select>
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Time</label>
-                        <input type="time" name="time" id="time" onChange={(e) => setTime(e.target.value)} value={time} placeholder="Time" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Calories</label>
-                        <input type="number" name="calories" id="calories" onChange={(e) => setCalories(e.target.value)} value={calories} placeholder="Calories" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Protein</label>
-                        <input type="number" name="protein" id="protein" onChange={(e) => setProtein(e.target.value)} value={protein} placeholder="Protein" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Carbs</label>
-                        <input type="number" name="carbs" id="carbs" onChange={(e) => setCarbs(e.target.value)} value={carbs} placeholder="Carbs" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Fats</label>
-                        <input type="number" name="fats" id="fats" onChange={(e) => setFats(e.target.value)} value={fats} placeholder="Fats" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Sugar</label>
-                        <input type="number" name="sugar" id="sugar" onChange={(e) => setSugar(e.target.value)} value={sugar} placeholder="Sugar" />
-                    </fieldset>
-                    <fieldset className={styles['half-input']}>
-                        <label>Sodium</label>
-                        <input type="number" name="sodium" id="sodium" onChange={(e) => setSodium(e.target.value)} value={sodium} placeholder="Sodium" />
-                    </fieldset>
+                        <input className={styles['half-input']} type="time" name="time" id="time" onChange={(e) => setTime(e.target.value)} value={time} placeholder="Time" />
+                        <input className={styles['half-input']} type="number" name="calories" id="calories" onChange={(e) => setCalories(e.target.value)} value={calories} placeholder="Calories" />
+                        <input className={styles['half-input']} type="number" name="protein" id="protein" onChange={(e) => setProtein(e.target.value)} value={protein} placeholder="Protein" />
+                        <input className={styles['half-input']} type="number" name="carbs" id="carbs" onChange={(e) => setCarbs(e.target.value)} value={carbs} placeholder="Carbs" />
+                        <input className={styles['half-input']} type="number" name="fats" id="fats" onChange={(e) => setFats(e.target.value)} value={fats} placeholder="Fats" />
+                        <input className={styles['half-input']} type="number" name="sugar" id="sugar" onChange={(e) => setSugar(e.target.value)} value={sugar} placeholder="Sugar" />
+                        <input className={styles['half-input']} type="number" name="sodium" id="sodium" onChange={(e) => setSodium(e.target.value)} value={sodium} placeholder="Sodium" />
                     <fieldset>
                         <label>Notes</label>
                         <input type="text" id="note-content" name="note-content" onChange={(e) => setNote(e.target.value)} value={note} placeholder="Notes" />
