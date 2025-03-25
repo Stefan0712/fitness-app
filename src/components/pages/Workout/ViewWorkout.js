a1d88dd7038bf3d7b78ed15754f60ff9a45f4624
Stefan0712 <s.vladulescu@gmail.com>
2025-03-25 13:44:04 +0200

    Updated View WOrkout to handle cases where the selected workout is not from user's library and added a temp loading screen

--- src/components/pages/Workout/ViewWorkout.js
+++ src/components/pages/Workout/ViewWorkout.js
@@ -6,6 +6,7 @@ import { getDateForHeader, makeFirstUpperCase } from '../../../helpers'
 import { useState, useEffect } from 'react';
 import { IconLibrary } from '../../../IconLibrary';
 import { exercises as databaseExercises } from '../../../database';
+import { workouts as databaseWorkouts} from '../../../database';
 
 
 //TODO: fix tags not properly showing when there are too many.
@@ -19,8 +20,12 @@ const ViewWorkout = () => {
     const [showConfirmDelete, setShowConfirmDelete] = useState(false)
 
     const [exercises, setExercises] = useState([]);
-    const workoutData = useSelector((state)=>state.user.workouts.find((item)=>item.id === id));
-    console.log(workoutData);
+    const libraryWorkouts = useSelector((state)=>state.user.workouts);
+
+    const libraryWorkoutIndex = libraryWorkouts.findIndex(item=>item.id===id);
+    const databaseWorkoutIndex = databaseWorkouts.findIndex(item=>item.id===id);
+
+    const workoutData = libraryWorkoutIndex >=0 ? libraryWorkouts[libraryWorkoutIndex] : databaseWorkoutIndex >=0 ? databaseWorkouts[databaseWorkoutIndex] : null;
     const libraryExercises = useSelector((state)=>state.user.exercises);
 
     
@@ -50,107 +55,121 @@ const ViewWorkout = () => {
     useEffect(()=>{
         fetchExercises();
     },[])
-    return ( 
-        <div className={styles['view-workout-page']}>
-            <div className='header'>
-                <div className='date'>{getDateForHeader()}</div>
-                <h2>{workoutData.name}</h2>
-                
-            </div>
-           <div className={styles['view-workout-content']}>
-           <div className={styles['workout-info']}>
-                <div className={`${styles['block']} ${styles['description-container']} ${styles['full-width']}`}>
-                    <div className={styles['header']}>
-                        <img className='small-icon' src={IconLibrary.InfoCircle} alt=''></img>
-                        <p>Description</p>
-                    </div>
-                    <p className={styles['value']}>{workoutData.description}</p>
+
+    if(workoutData){
+
+        return ( 
+            <div className={styles['view-workout-page']}>
+                <div className='header'>
+                    <div className='date'>{getDateForHeader()}</div>
+                    <h2>{workoutData.name}</h2>
+                    
                 </div>
-                <div className={`${styles['block']} ${styles.groups}`}>
-                    <div className={styles['header']}>
-                        <img className='small-icon white-icon' src={IconLibrary.Tag} alt=''></img>
-                        <p className={styles['name']}>Tags</p>
-                    </div>
-                    <div className={`${styles['tags-container']}`}>
-                        {workoutData.tags?.length > 0 ? workoutData.tags.map(tag=>
-                        (
-                            <div className={styles["tag-body"]} key={tag.id}>
-                                <div className={styles["tag-color"]} style={{backgroundColor: tag.color}}></div>
-                                <div className={styles["tag-name"]}>{tag.name}</div>
-                            </div>
-                        )) : 'None'}
-                    </div>
-                </div>
-                <div className={`${styles['block']} ${styles.equipments}`}>
-                    <div className={styles['header']}>
-                        <img className='small-icon white-icon' src={IconLibrary.Dumbbell} alt=''></img>
-                        <p className={styles['name']}>Equipment</p>
-                    </div>
-                    <div className={`${styles['equipments-container']}`}>
-                        {workoutData.equipment && workoutData.equipment?.length > 0 ? workoutData.equipment.map(eq=>(
-                        <div className={styles["equipment"]} key={eq.id}>
-                            <div className={styles["equipment-name"]}>{eq.name}</div>
+               <div className={styles['view-workout-content']}>
+               <div className={styles['workout-info']}>
+                    <div className={`${styles['block']} ${styles['description-container']} ${styles['full-width']}`}>
+                        <div className={styles['header']}>
+                            <img className='small-icon' src={IconLibrary.InfoCircle} alt=''></img>
+                            <p>Description</p>
                         </div>
-                        )) : (
-                            <div className={styles["equipment"]} key={'default equipment'}>
-                                <div className={styles["equipment-name"]}>None</div>
+                        <p className={styles['value']}>{workoutData.description}</p>
+                    </div>
+                    <div className={`${styles['block']} ${styles.groups}`}>
+                        <div className={styles['header']}>
+                            <img className='small-icon white-icon' src={IconLibrary.Tag} alt=''></img>
+                            <p className={styles['name']}>Tags</p>
+                        </div>
+                        <div className={`${styles['tags-container']}`}>
+                            {workoutData.tags?.length > 0 ? workoutData.tags.map(tag=>
+                            (
+                                <div className={styles["tag-body"]} key={tag.id}>
+                                    <div className={styles["tag-color"]} style={{backgroundColor: tag.color}}></div>
+                                    <div className={styles["tag-name"]}>{tag.name}</div>
+                                </div>
+                            )) : 'None'}
+                        </div>
+                    </div>
+                    <div className={`${styles['block']} ${styles.equipments}`}>
+                        <div className={styles['header']}>
+                            <img className='small-icon white-icon' src={IconLibrary.Dumbbell} alt=''></img>
+                            <p className={styles['name']}>Equipment</p>
+                        </div>
+                        <div className={`${styles['equipments-container']}`}>
+                            {workoutData.equipment && workoutData.equipment?.length > 0 ? workoutData.equipment.map(eq=>(
+                            <div className={styles["equipment"]} key={eq.id}>
+                                <div className={styles["equipment-name"]}>{eq.name}</div>
+                            </div>
+                            )) : (
+                                <div className={styles["equipment"]} key={'default equipment'}>
+                                    <div className={styles["equipment-name"]}>None</div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className={`${styles['block']} ${styles.groups}`}>
+                        <div className={styles['header']}>
+                            <img className='small-icon white-icon' src={IconLibrary.Muscle} alt=''></img>
+                            <p className={styles['name']}>Target Muscles</p>
+                        </div>
+                        <p className={`${styles['groups-container']}`}>{workoutData.targetGroup?.length > 0 ? workoutData.targetGroup.map((group,index) =><p key={'group '+index}>{group.name}</p>  ) : 'None'}</p>
+                    </div>
+                    <div className={styles['block']}>
+                        <div className={styles['header']}>
+                            <img className='small-icon white-icon' src={IconLibrary.Dumbbell} alt=''></img>
+                            <p className={styles['name']}>Difficulty</p>
+                        </div>
+                        <p className={styles['value']}>{workoutData.difficulty ? makeFirstUpperCase(workoutData.difficulty) : 'Not set'}</p>
+                    </div>
+                    <div className={styles['block']}>
+                        <div className={styles['header']}>
+                            <img className='small-icon white-icon' src={IconLibrary.Link} alt=''></img>
+                            <p className={styles['name']}>Reference (url)</p>
+                        </div>
+                        <p className={styles['value']}>{workoutData.reference ? workoutData.reference : 'Not Set'}</p>
+                    </div> 
+                </div>
+                <h3 className='subtitle full-width'>Exercises</h3>
+                <div className={styles['workout-exercises']}>
+                  {exercises?.length > 0 ? exercises.map((exercise, index)=>(
+                        <div className={styles['exercise-body']} key={index+'ex'}>
+                            <p className={styles['exercise-index']}>{index+1}</p>
+                            <b className={styles['exercise-name']}>{exercise.name}</b>
+                            <p className={styles['exercise-sets']}>{exercise.sets} sets</p>
+                        </div>
+                  )) : (<p>Loading Exercises</p>)}
+                <div className={styles['buttons']}>
+                    <div className={styles['menu-buttons']}>
+                        {showConfirmDelete ? (
+                            <div className={styles['buttons-container']}>
+                                <button className={styles['menu-button']} onClick={deleteWorkout}><img className='small-icon' src={IconLibrary.Yes} alt=''/></button>
+                                <div className={styles['divider']} />
+                                <button className={styles['menu-button']} onClick={()=>setShowConfirmDelete(false)}><img className='small-icon' src={IconLibrary.No} alt=''/></button>
+                            </div>
+                        ):(
+                            <div className={styles['buttons-container']}>
+                                <button className={styles['menu-button']} onClick={()=>setShowConfirmDelete(true)}>Delete</button>
+                                <div className={styles['divider']} />
+                                <Link  className={styles['menu-button']} to={`/workout/${workoutData.id}/edit`}>Edit</Link>
                             </div>
                         )}
                     </div>
+                    <Link to={`/workout/${workoutData.id}/start`} className={`orange-button large-button ${styles['start-workout-button']}`}>Start Workout</Link>
                 </div>
-                <div className={`${styles['block']} ${styles.groups}`}>
-                    <div className={styles['header']}>
-                        <img className='small-icon white-icon' src={IconLibrary.Muscle} alt=''></img>
-                        <p className={styles['name']}>Target Muscles</p>
-                    </div>
-                    <p className={`${styles['groups-container']}`}>{workoutData.targetGroup?.length > 0 ? workoutData.targetGroup.map((group,index) =><p key={'group '+index}>{group.name}</p>  ) : 'None'}</p>
                 </div>
-                <div className={styles['block']}>
-                    <div className={styles['header']}>
-                        <img className='small-icon white-icon' src={IconLibrary.Dumbbell} alt=''></img>
-                        <p className={styles['name']}>Difficulty</p>
-                    </div>
-                    <p className={styles['value']}>{workoutData.difficulty ? makeFirstUpperCase(workoutData.difficulty) : 'Not set'}</p>
-                </div>
-                <div className={styles['block']}>
-                    <div className={styles['header']}>
-                        <img className='small-icon white-icon' src={IconLibrary.Link} alt=''></img>
-                        <p className={styles['name']}>Reference (url)</p>
-                    </div>
-                    <p className={styles['value']}>{workoutData.reference ? workoutData.reference : 'Not Set'}</p>
-                </div> 
+               </div>
             </div>
-            <h3 className='subtitle full-width'>Exercises</h3>
-            <div className={styles['workout-exercises']}>
-              {exercises?.length > 0 ? exercises.map((exercise, index)=>(
-                    <div className={styles['exercise-body']} key={index+'ex'}>
-                        <p className={styles['exercise-index']}>{index+1}</p>
-                        <b className={styles['exercise-name']}>{exercise.name}</b>
-                        <p className={styles['exercise-sets']}>{exercise.sets} sets</p>
-                    </div>
-              )) : (<p>Loading Exercises</p>)}
-            <div className={styles['buttons']}>
-                <div className={styles['menu-buttons']}>
-                    {showConfirmDelete ? (
-                        <div className={styles['buttons-container']}>
-                            <button className={styles['menu-button']} onClick={deleteWorkout}><img className='small-icon' src={IconLibrary.Yes} alt=''/></button>
-                            <div className={styles['divider']} />
-                            <button className={styles['menu-button']} onClick={()=>setShowConfirmDelete(false)}><img className='small-icon' src={IconLibrary.No} alt=''/></button>
-                        </div>
-                    ):(
-                        <div className={styles['buttons-container']}>
-                            <button className={styles['menu-button']} onClick={()=>setShowConfirmDelete(true)}>Delete</button>
-                            <div className={styles['divider']} />
-                            <Link  className={styles['menu-button']} to={`/workout/${workoutData.id}/edit`}>Edit</Link>
-                        </div>
-                    )}
+         );
+    }else{
+        return(
+            <div className="page workout-page">
+                <div className="header">
+                    <div className="date">{getDateForHeader()}</div>
+                    <h2>Exercise loading</h2>
                 </div>
-                <Link to={`/workout/${workoutData.id}/start`} className={`orange-button large-button ${styles['start-workout-button']}`}>Start Workout</Link>
+                <h1>Loading...</h1>
             </div>
-            </div>
-           </div>
-        </div>
-     );
+        )
+    }
 }
  
 export default ViewWorkout;
