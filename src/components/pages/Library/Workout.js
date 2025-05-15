82f9b0c826cdf2b350575583e14d674e9f6e1a4c
Stefan0712 <s.vladulescu@gmail.com>
2025-05-15 19:32:39 +0300

    fixed the number of exercises not showing correctly for workouts having phases instead of a single exercises list

--- src/components/pages/Library/Workout.js
+++ src/components/pages/Library/Workout.js
@@ -4,12 +4,27 @@ import { Link } from 'react-router-dom';
 import { makeFirstUpperCase } from '../../../helpers';
 
 const Workout = ({workout, id, type}) => {
+
+    const getNumberOfExercises = () => {
+        if(workout.phases){
+            let sum = 0;
+            if (workout.phases) {
+                workout.phases.forEach(phase => {
+                sum += phase.exercises?.length || 0;
+                });
+            }
+            return sum;
+        }else if(workout.exercises){
+            return workout.exercises.length;
+        }
+    };
+
     return ( 
         <Link to={`/workout/${id}/view/${type === 'online' ? '?type=online' : ''}`} className={`${styles["item-body"]} ${styles['workout-body']}`}>
             <div className={`${styles["item-info"]}`}>
                 <h4>{workout.name}</h4>
                 <div className={styles["item-description"]}>
-                    <p>{workout.exercises?.length || 0} exercises</p>
+                    <p>{getNumberOfExercises() || 0} exercises</p>
                     <p>{makeFirstUpperCase(workout.difficulty || '')}</p>
                 </div>
                 <div className={styles['workout-tags']}>
