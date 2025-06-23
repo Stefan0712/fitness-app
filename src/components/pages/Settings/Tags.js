a086ed2fb125a771f952181e5734e2dfbcc3184a
Stefan0712 <s.vladulescu@gmail.com>
2025-06-23 21:21:39 +0300

    Created a separate Tags page, updated the correct link in Menu, added basic functionality

--- src/components/pages/Settings/Tags.js
+++ src/components/pages/Settings/Tags.js
@@ -1,84 +1,110 @@
-import CustomItem from '../../common/CustomItem/CustomItem';
-import styles from './Settings.module.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { getDateForHeader } from '../../../helpers';
 import { useEffect, useState } from 'react';
-import CustomItemCreator from '../../common/CustomItemCreator/CustomItemCreator';
+import {deleteItem, getAllItems, saveItem} from '../../../db';
+import {useUI} from '../../../context/UIContext.jsx';
+import styles from './Tags.module.css';
+import {IconLibrary} from '../../../IconLibrary.js';
+import AppHeader from '../../common/AppHeader/AppHeader.tsx';
+import { getUser } from '../../../auth.ts';
+import ColorPicker from '../../common/ColorPicker/ColorPicker.tsx';
+import {v4 as uuidv4} from 'uuid';
 
+
+
+// TODO: Search existing tags when entering a name into new Tag form
 const Tags = () => {
 
-    const dispatch = useDispatch();
+    const {showMessage, showConfirmationModal} = useUI();
+    const [selectedTag, setSelectedTag] = useState(null);
+    const [editTag, setEditTag] = useState(null);
 
-    const tags = [];
+    const [tags, setTags] = useState([]);
 
-    const [defaultTags, setDefaultTags] = useState([]);
-    const [userTags, setUserTags] = useState([]);
+    const getTags = async () =>{
+        try{
+            const result = await getAllItems('tags');
+            setTags(result)
+        }catch(error){
+            console.error(error);
+        }
+    };
+    useEffect(()=>{getTags()},[]);
 
 
-    const [screenToShow, setScreenToShow] = useState('custom')
 
-
-
-
-    useEffect(()=>{
-        console.log(tags)
-        let defaultTags = [];
-        let userTags = [];
-        tags.forEach((tag)=>{
-            if(tag.source === "system"){
-                defaultTags.push(tag)
-            }else if(tag.source === 'user'){
-                userTags.push(tag)
-            }
-        })
-        setDefaultTags(defaultTags);
-        setUserTags(userTags);
-    },[tags])
-
-    const createTag = (tag) =>{
-        console.log("Created tag")
-    }
-    const deleteTag = (id)=>{
-        console.log("Deleted tag")
-
-    }
-    const editTag = (data) =>{
-        console.log("Edit tag")
+    const handleDelete = async () =>{
+        if(selectedTag){
+            await deleteItem('tags', selectedTag._id);
+            showMessage('Tag deleted successfully!', 'success');
+            setSelectedTag(null);
+            setTags(prev=>[...prev.filter(item=>item._id!==selectedTag._id)]);
+        }
     }
     return ( 
-        <div className={`${styles['tags-page']} ${styles['custom-items-page']} page`}>
-             <div className='header'>
-                <div className='date'>{getDateForHeader()}</div>
-                <h2>Manage Tags</h2>
-            </div>
-            <CustomItemCreator key='custom-items-fields' addItem={createTag} type={'tag'} />
-            <div className="screen-toggle-buttons">
-                <button onClick={()=>setScreenToShow('custom')} className={screenToShow === 'custom' ? 'selected-button' : ''}>Custom Tags ({userTags.length})</button>
-                <button onClick={()=>setScreenToShow('default')} className={screenToShow === 'default' ? 'selected-button' : ''}>Default Tags ({defaultTags.length})</button>
-            </div>
-            <div className='screens-container'>
-                {screenToShow === 'custom' ? (
-                    <div className={`screen left`}>
-                        {userTags?.map((tag)=>(
-                            <CustomItem key={tag.id} data={tag} />
-                        ))}  
-                    </div>  
-                ):null}  
-                {screenToShow === "default" ? (
-                    <div className={`screen right`}>
-                        {defaultTags?.map((tag)=>(
-                            <CustomItem key={tag.id} data={tag} deleteItem={deleteTag} updateItem={editTag} />
-                        ))}  
-                    </div>  
-                ) : null}
-              
-            </div>
-            
-            
-                 
-                    
+        <div className={styles.tagsPage}>
+             <AppHeader title={'Tags'} />
+             <div className={styles.menu}>
+                {selectedTag ? 
+                    <div className={styles.menuButtons}>
+                        <button className={styles.addTagButton} onClick={()=>showConfirmationModal({title: "Are you sure?", message: 'This will delete your tag and cannot be undone. Do you want to continue?', onConfirm: ()=>handleDelete()})}><img src={IconLibrary.Delete} alt='' className='small-icon'/></button>
+                        <button className={styles.addTagButton} onClick={()=>setEditTag(selectedTag)}><img src={IconLibrary.Edit} alt='' className='small-icon'/></button>
+                    </div> 
+                : null}
+             </div>
+             <div className={styles.tagsContainer}>
+                {tags && tags.length > 0 ? tags.map(item=><Tag tag={item} key={item._id+item.name} selectTag={setSelectedTag} selectedTag={selectedTag} />) : <p>No tags</p>}
+             </div>
+             <TagForm key={editTag ? editTag._id : "Tag-form-key"} tag={editTag} addTag={setTags} clearSelected={()=>setSelectedTag(null)} />
         </div>
      );
 }
- 
 export default Tags;
+
+
+const Tag = ({tag, selectedTag, selectTag}) =>{
+    return (
+        <div className={`${styles.tag} ${selectedTag ? styles.selectedTag : ''}`} onClick={()=>selectTag(prev=> prev && prev._id === tag._id ? null : tag)}>
+            <div style={{width: '30px', height: '30px', backgroundColor: tag.color, borderRadius: '5px'}}></div>
+            <h3>{tag.name}</h3>
+        </div>
+    )
+}
+
+const TagForm = ({tag, addTag, clearSelected}) => {
+
+    const {showMessage} = useUI();
+    const [name, setName] = useState(tag ? tag.name : '');
+    const [color, setColor] = useState(tag ? tag.color :  '#FFFFFF');
+    const [showColorPicker, setShowColorPicker] = useState(false);
+    const [editMode, setEditMode] = useState(tag ? true : false);
+
+
+    
+    const handleAddTag = () =>{
+        if(name && name.length > 0){
+            const newTag = {
+                _id: tag ? tag._id :  uuidv4(),
+                author: tag ? tag.author :  getUser(),
+                name,
+                color
+            }
+            saveItem('tags', newTag);
+            addTag(prev=>[...prev.filter(item=>item._id!==newTag._id), newTag]);
+            showMessage(editMode ? 'Tag updated successfully' : 'Tag created successfully!', 'success');
+            setName('');
+            setColor('#FFFFFF');
+            clearSelected();
+            if(editMode){
+                setEditMode(false);
+            };
+        }
+    }
+    return(
+        <div className={styles.tagForm}>
+            <input type='text' id='name' name='name' onChange={(e)=>setName(e.target.value)} value={name} placeholder='Tag name...'/>
+            {showColorPicker ? <ColorPicker getColor={setColor} currentColor={color} closeModal={()=>setShowColorPicker(false)}/> : null }
+            <div onClick={()=>setShowColorPicker(prev=>!prev)} className={styles.colorButton} style={{backgroundColor: color}}></div>
+            <button className={styles.addTagButton} onClick={handleAddTag}><img src={editMode ? IconLibrary.Save : IconLibrary.Add} alt='' /></button>
+        </div>
+    )
+    
+}
